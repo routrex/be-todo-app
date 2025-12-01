7e77cc5e1583b17584e37debd8c354ac590efcb2
routrex <rifaldiekaputra637@gmail.com>
2025-12-01 14:13:01 +0700

    update deploy to vercel

--- server.js
+++ server.js
@@ -17,5 +17,4 @@ connectDB();
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app
